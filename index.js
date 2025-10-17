93272d5649e33fe65bf357496d3e02ac3b386ca6
Obed-Is <obedaquino71@gmail.com>
2025-10-16 20:47:27 -0600

    Conexion a mySql con mysql2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import 'dotenv/config';
 import { getMongo } from './src/connection_mongo.js';
 import { getSqlServer } from './src/connection_sqlServer.js';
+import { getMySql } from './src/connection_mySql.js';
 
 const app = express();
 
 //CONEXIONES
 await getMongo();
 await getSqlServer();
+await getMySql();
 
 
 app.listen(process.env.SERVER_PORT || 4000,
---
+++ src/connection_mySql.js
@@ -0,0 +1,25 @@
+import mySql from 'mysql2';
+
+const configMySql = {
+    host: process.env.DB_HOST_MS,
+    port: process.env.DB_PORT_MS,
+    database: process.env.DB_NAMEDB_MS,
+    user: process.env.DB_USER_MS,
+    password: process.env.DB_PASSW_MS,
+    connectionLimit :10
+}
+
+export const getMySql = async () => {
+    try {
+        const connection = await mySql.createConnection(configMySql);
+
+        connection.on('error',(err) => {
+            console.error(err);
+        })
+
+        console.log('Conexion a mySql exitosa 😎');
+    } catch (error) {
+        console.log('Ocurrio un error al intentar conectarse a mySql Server 💀')
+        console.error(error.message);
+    }
+}
